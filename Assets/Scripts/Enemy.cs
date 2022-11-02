0eba1f3cdc2518010f35d6700189ab0ec7962f04
MarleneMo <marlene8810@gmail.com>
2022-11-03 01:16:29 +0800

    no message

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -2,33 +2,37 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Enemy : MonoBehaviour
 {
-    private float hp = 100f;
-    // Start is called before the first frame update
+    public GameObject player;
+    private float hp = 10f;
+
     void Start()
     {
+      
+     }
 
-    }
-
-    // Update is called once per frame
     void Update()
     {
+        //敵人追逐玩家
+        Quaternion targtRotation = 
+        transform.position += transform.forward * 1f * Time.deltaTime;
+     }
 
-    }
     void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Bullet")
         {
             //先扣血
-            hp -= 25;
+            hp -= 10;
             if (hp <= 0)
             {
-                //刪除物件 設為停用、並移除 
+                //刪除物件 設為停用、並移除
+
                 other.gameObject.SetActive(false);
                 Destroy(this.gameObject);
             }
-
         }
     }
 }
