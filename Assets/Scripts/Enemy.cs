081de21e4f1fcbe3e97a59b702a82d757e4e1c69
MarleneMo <marlene8810@gmail.com>
2022-11-03 01:27:20 +0800

    更新敵人模型

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -2,23 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-
 public class Enemy : MonoBehaviour
 {
     public GameObject player;
+
     private float hp = 10f;
 
     void Start()
     {
-      
-     }
+    }
 
     void Update()
     {
         //敵人追逐玩家
-        Quaternion targtRotation = 
+        Quaternion targtRotation =
+            Quaternion
+                .LookRotation(player.transform.position - transform.position);
+        transform.rotation =
+            Quaternion
+                .Slerp(transform.rotation, targtRotation, 1 * Time.deltaTime);
         transform.position += transform.forward * 1f * Time.deltaTime;
-     }
+    }
 
     void OnTriggerEnter(Collider other)
     {
@@ -29,7 +33,6 @@ public class Enemy : MonoBehaviour
             if (hp <= 0)
             {
                 //刪除物件 設為停用、並移除
-
                 other.gameObject.SetActive(false);
                 Destroy(this.gameObject);
             }
